5a2be95847a5b24575a65c289e30f2051da2ca14
Jay <Nopenotj@gmail.com>
2020-05-21 15:24:58 +0800

    fixed routing of dishes TODO: tags

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,10 +1,16 @@
 class DishesController < ApplicationController
+
   def index
-    dishes = Dish.all
+    dishes = get_restaurant.dishes
     render json: DishSerializer.new(dishes).serialized_json
   end
   def show
     dish = Dish.find(params[:id])
     render json: DishSerializer.new(dish).serialized_json
+  end
+
+  private
+  def get_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
