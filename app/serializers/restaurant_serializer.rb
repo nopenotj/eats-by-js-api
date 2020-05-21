0a4d86ac3113a429050312a4bc1cefdf664b35e8
Jay <Nopenotj@gmail.com>
2020-05-21 11:25:49 +0800

    minor change to serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,7 @@
 class RestaurantSerializer
     include FastJsonapi::ObjectSerializer
     attributes :title, :description, :rating
+
+    has_many :dishes, dependent: :destroy
+    has_many :tags 
   end
