3ffc84c2e8e9c49b6687dbc9663b39a8b936a3f2
Jay <Nopenotj@gmail.com>
2020-05-25 18:24:16 +0800

    added created_at to review model

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :title, :content, :rating
+    attributes :title, :content, :rating, :created_at
     has_one :restaurant
     has_one :user
   end
