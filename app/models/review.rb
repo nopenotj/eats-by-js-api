d6a42bc0241d23c7e7213b4889a0768a31972def
Jay <Nopenotj@gmail.com>
2020-05-25 21:28:26 +0800

    added validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,6 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :rating, presence: true
+
+  validates_numericality_of :rating, integer:true, greater_than_or_equal: 0, less_than_or_equal_to: 5
 end
