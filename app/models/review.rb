cbed104e2d44ac774adf40bde7466885b4d76a97
Jay <Nopenotj@gmail.com>
2020-05-25 18:00:39 +0800

    added validations on model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
 end
