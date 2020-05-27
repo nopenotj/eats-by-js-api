9a6f29a30bb463cb31c8b47c6152a190795ce273
Jay <Nopenotj@gmail.com>
2020-05-27 17:36:53 +0800

    made restaurant rating based on reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,4 +8,15 @@ class Restaurant < ApplicationRecord
     has_many :reviews
     has_many :dishes, dependent: :destroy
     has_and_belongs_to_many :tags 
+
+    def rating 
+      all_reviews = self.reviews
+      sum = all_reviews.map{|x| x.rating}.reduce(:+) 
+      if sum.nil? 
+        -1
+      else
+        count = all_reviews.count.to_f
+        sum / count
+      end
+    end
 end
