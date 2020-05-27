e2c40800cc2edeed59ec3f8ed3da1f78a5ef2b12
Jay <Nopenotj@gmail.com>
2020-05-27 17:41:11 +0800

    expose price attribute of restaurant

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,6 +1,6 @@
 class RestaurantSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :title, :description, :rating, :image_link
+    attributes :title, :description, :rating, :price, :image_link
 
     has_many :dishes, dependent: :destroy
     has_many :tags 
