c78a8893d47d46f745730ce2cc2a1ed35a25f81a
Jay <Nopenotj@gmail.com>
2020-05-29 02:40:59 +0800

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# # Restaurants
-# jay_res = Restaurant.create( title: "Jay's toppokki Restaurant", description: "best toppoki in the world", price: 5)
-# jerryl_res = Restaurant.create( title: "Jerryl's toppokki Restaurant", description: "best toppoki in the world", price: 5)
-# aer_res = Restaurant.create( title: "Aerin's toppokki Restaurant", description: "worst toppoki in the world", price: 1)
-# 
-# # Dishes
-# jay_res.dishes.create( title: "jay's toppoki", description: "best toppoki in the world", price: 69) 
-# jerryl_res.dishes.create( title: "jerryl's toppoki", description: "best toppoki in the world", price: 69)
-# aer_res.dishes.create( title: "aerin's toppoki", description: "worst toppoki in the world", price: 2)
-# 
-# # Tags
-# chinese = Tag.create(name: "chinese")
-# korean = Tag.create(name: "korean")
-# noodles = Tag.create(name: "noodles")
-# 
-# jay_res.tags << [chinese,korean,noodles]
-# jerryl_res.tags << [chinese,korean,noodles]
-# aer_res.tags << [chinese,korean,noodles]
-# 
-# jay_res.update(image_link: "https://letyourheartspeaks.files.wordpress.com/2018/01/image6.jpeg?w=1060" )
-# jerryl_res.update(image_link: "https://www.capitaland.com/content/dam/capitaland-malaysia-tenants/the-mines/myeongdong-topokki.jpg.transform/cap-lowres/image.jpg" )
-# aer_res.update(image_link: "https://www.gardeningknowhow.com/wp-content/uploads/2019/04/lawn-pH.jpg" )
+# Restaurants
+jay_res = Restaurant.create( title: "Jay's toppokki Restaurant", description: "best toppoki in the world", price: 5)
+jerryl_res = Restaurant.create( title: "Jerryl's toppokki Restaurant", description: "best toppoki in the world", price: 5)
+aer_res = Restaurant.create( title: "Aerin's toppokki Restaurant", description: "worst toppoki in the world", price: 1)
+
+# Dishes
+jay_res.dishes.create( title: "jay's toppoki", description: "best toppoki in the world", price: 69) 
+jerryl_res.dishes.create( title: "jerryl's toppoki", description: "best toppoki in the world", price: 69)
+aer_res.dishes.create( title: "aerin's toppoki", description: "worst toppoki in the world", price: 2)
+
+# Tags
+chinese = Tag.create(name: "chinese")
+korean = Tag.create(name: "korean")
+noodles = Tag.create(name: "noodles")
+
+jay_res.tags << [chinese,korean,noodles]
+jerryl_res.tags << [chinese,korean,noodles]
+aer_res.tags << [chinese,korean,noodles]
+
+jay_res.update(image_link: "https://letyourheartspeaks.files.wordpress.com/2018/01/image6.jpeg?w=1060" )
+jerryl_res.update(image_link: "https://www.capitaland.com/content/dam/capitaland-malaysia-tenants/the-mines/myeongdong-topokki.jpg.transform/cap-lowres/image.jpg" )
+aer_res.update(image_link: "https://www.gardeningknowhow.com/wp-content/uploads/2019/04/lawn-pH.jpg" )
 
 
 # {
@@ -45,6 +45,7 @@ data_hash = JSON.parse(file)
 data_hash["restaurants_data"].each { |restaurant|
   res = Restaurant.create( 
                           title: restaurant["title"],
+                          image_link: restaurant["image_link"]
                           description: "NUS Store", 
                           location: restaurant["location"],
                           operating_hours: restaurant["operating_hours"],
