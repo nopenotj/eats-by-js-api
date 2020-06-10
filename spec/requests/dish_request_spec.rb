701e02eca393c4184fd5a9941507fda8f540effb
Jay <Nopenotj@gmail.com>
2020-06-10 17:28:00 +0800

    wrote some tests for dish controller

--- spec/requests/dish_request_spec.rb
+++ spec/requests/dish_request_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Dishes", type: :request do
+  describe "#index" do
+    context "when GET request is made to /dishes" do
+      it "should return with OK response" do
+        get dishes_path
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 
+  describe "#show" do
+    # need test for HTTP STATUS 200 OK
+    context "when GET request is made to /dishes/:id" do
+      let(:dish) { create :dish }
+      it "should return with OK response if exists" do
+        get dishes_path(dish.id)
+        expect(response).to have_http_status(:ok)
+      end
+      it "should return with 404 NOT_FOUND response if does not exist" do
+        # server must respond with 404 not found when processing request to fetch single resource that doesnt exists
+        # unless 200 OK with null as primary data
+        dish
+        get dish_path(Dish.last.id + 1)
+        expect(response).to have_http_status(:not_found)
+      end
+      pending "should contain valid JSON content"
+
+    end
+  end
 end
