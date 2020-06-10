2289790842e0e9dbdde8ade0a767b4d2258a1fba
Jay <Nopenotj@gmail.com>
2020-06-10 13:19:11 +0800

    wrote review model spec

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(build(:review)).to be_valid
+  end
+
+  let(:review) { build(:review) }
+
+  describe "ActiveModel validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_numericality_of(:rating) }
+  end
+
+  describe "ActiveRecord associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:restaurant) }
+  end
+
 end
