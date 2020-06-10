aa78d6ade68cead48fbcae5807f6e2a4db7c39ca
Jay <Nopenotj@gmail.com>
2020-06-10 13:04:20 +0800

    add default behvaior for record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   attr_reader :current_user
 
   def authenticate_request
     @current_user = AuthorizeApiRequest.call(request.headers).result
     render json: { errors: 'Not Authorized' }, status: 401 unless @current_user
+  end
+
+  private
+
+  def record_not_found
+    render status: 404
   end
 end
