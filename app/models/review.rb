db363539bb19b3ef1670e656965ce2a8b12563f2
Jay <Nopenotj@gmail.com>
2020-06-20 15:55:52 +0800

    remove title validation from review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
-  validates :title, presence: true
   validates :content, presence: true
   validates :rating, presence: true
 
